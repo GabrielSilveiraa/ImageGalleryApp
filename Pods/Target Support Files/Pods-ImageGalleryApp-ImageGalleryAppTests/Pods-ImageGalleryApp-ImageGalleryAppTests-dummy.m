bb3949a9f9c3cfed1ac8412182b757c84d19cc60
Gabriel Silveira <silveira_gabriel@sicredi.com.br>
2020-05-11 14:11:57 -0300

    Created project base

---
+++ Pods/Target Support Files/Pods-ImageGalleryApp-ImageGalleryAppTests/Pods-ImageGalleryApp-ImageGalleryAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ImageGalleryApp_ImageGalleryAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_ImageGalleryApp_ImageGalleryAppTests
+@end
